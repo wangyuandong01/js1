221d6ba1c67fa7da38320706ecd06806a4cc7e38
wangyundong01 <18845633271@163.com>
2017-07-13 21:25:57 +0800

    jquare

--- 1.js
+++ 1.js
@@ -2,3 +2,15 @@
  * Created by 东东哥 on 2017/7/11.
  */
 
+function cloneObj(obj) {
+    var newObj = {};
+    for (var p in obj) {
+        if (typeof obj[p] === 'object') {//判断元素中是否还有对象
+            newObj[p] = arguments.callee(obj[p]);//如果有将递归调用自己argument.callee
+            //newObj[p] = cloneObj(obj[p]);
+        } else {
+            newObj[p] = obj[p];
+        }
+    }
+    return newObj;
+}
